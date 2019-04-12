289f4f6c8192edb7db3006080f519e0bdd735270
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-04-12 15:26:21 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :product, only:[:index] do
+    get '/add', to: "product#add"
+  end
+
+  get '/', to: "application#index"
+  post '/', to: "application#add_to_cart"
 end
